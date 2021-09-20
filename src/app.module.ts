a3c1e6f883bbf068e6a9acc211f3bf5b15879906
koji koyama <k.koyama37@gmail.com>
2021-09-20 22:19:01 +0900

    feat: graphqlの初期設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
