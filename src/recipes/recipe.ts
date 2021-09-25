7186389c357b4c308d4183e725d29055118f686c
koji koyama <k.koyama37@gmail.com>
2021-09-25 23:43:09 +0900

    feat: Recipe クラスの内容定義。

--- src/recipes/recipe.ts
+++ src/recipes/recipe.ts
@@ -1,1 +1,10 @@
-export class Recipe {}
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class Recipe {
+  @Field((type) => ID)
+  id: string;
+
+  @Field()
+  title: string;
+}
